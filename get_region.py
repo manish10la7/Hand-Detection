d0558350033eb44884eb42d2e649bb8ddb926328
Manish Shrestha <Manish10la7@gmail.com>
2024-10-21 00:56:41 +0545

    This code identifies region, uses grayscale, and gaussian blur to identify elements from the frame

---
+++ get_region.py
@@ -0,0 +1,8 @@
+def get_region(frame):
+    # Separatinh the region of interest from the rest of the frame.
+    region = frame[region_top:region_bottom, region_left:region_right]
+    # Making it grayscale so we can detect the edges more easily.
+    region = cv2.cvtColor(region, cv2.COLOR_BGR2GRAY)
+    # Using a Gaussian blur to prevent frame noise from being labeled as an edge.
+    region = cv2.GaussianBlur(region, (5, 5), 0)
+    return region
